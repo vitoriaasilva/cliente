efd0d3ca32b1527853ae664852dab0c46e771904
vitoriaasilva <vitoriasilva160408@gmail.com>
2026-05-11 11:00:17 -0300

    corrigindo deploy 2

--- cliente/settings.py
+++ cliente/settings.py
@@ -127,4 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
